2c67b296f08c3c53f84fadc551fd055bec75f19c
Newton Ni <nwtnni@gmail.com>
2018-04-11 23:39:32 -0400

    No unwrap

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,15 @@ pub fn main() {
 
     let mut tmdb = TMDB::default();
     let _ = WriteLogger::init(LevelFilter::Info, Config::default(), File::create("movies.log").unwrap());
-    let _ = create_dir("posters").unwrap();
-    let _ = create_dir("movies").unwrap();
+    let _ = create_dir("posters");
+    let _ = create_dir("movies");
     let mut index = File::create("movies.json").unwrap();
-    index.write(b"[\n").unwrap();
+    let _ = index.write(b"[\n");
 
     for n in 1..PAGES {
-        if let Ok(page) = tmdb.get_page(n) {
+        match tmdb.get_page(n) {
+        | Err(error) => error!("Page error: {}", error),
+        | Ok(page) => {
             for id in page {
                 match Movie::save(id, &mut tmdb) {
                 | Err(warning) => warn!("{}", warning),
@@ -40,8 +42,9 @@ pub fn main() {
                 }
                 }
             }
+        },
         }
     }
 
-    index.write(b"\n]").unwrap();
+    let _ = index.write(b"\n]");
 }
